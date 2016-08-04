d862a80571f681334ef124ada0159b9f32d2c8bf
nguyenthingocdieu <ngocdieunguyen.ece@gmail.com>
2016-08-04 16:58:36 +0700

    Update

---
+++ TA-Dashboard/PageObjects/CommonActions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using TA_Dashboard.Common;
+
+
+namespace TA_Dashboard.PageObjects
+{
+    public class CommonActions
+    {
+        public void Click(By control)
+        {
+            Constant.driver.FindElement(control).Click();
+        }
+        public void Type(By control, string value)
+        {
+            Constant.driver.FindElement(control).Clear();
+            Constant.driver.FindElement(control).SendKeys(value);
+        }
+        public string GetTextOfControl(By control)
+        {
+            return Constant.driver.FindElement(control).Text;
+        }
+    }
+}
