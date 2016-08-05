bdbdc8a390b29372756c5e42c0c713619e74f897
ngantruong1801 <truongkimngan1801@gmail.com>
2016-08-05 11:29:23 +0700

    edit

--- TA-Dashboard/TestCases/BaseTest.cs
+++ TA-Dashboard/TestCases/BaseTest.cs
@@ -7,12 +7,29 @@ namespace TA_Dashboard.TestCases
     [TestClass]
     public class BaseTest : CommonActions
     {
+        public TestContext TestContext { get; set; }
+        MainPage mainpage = new MainPage();
+
         [AssemblyInitialize]
         public static void AssemblyInitializeMeThod(TestContext testContext)
         {
             CommonActions.OpenBrowser("firefox");
         }
 
+        [TestCleanup]
+        public void TestCleanup()
+        {
+
+            switch (TestContext.TestName)
+            {
+                case "":
+                    break;
+
+                default:
+                    mainpage.Logout();
+                    break;
+            }
+        }
 
         [AssemblyCleanup]
         public static void AssemblyCleapUpMethod()
