587cbf7182b901709e1e5f51302c9aa362679aac
canhtranngoc <canh.tran.bm@gmail.com>
2016-08-05 08:51:47 +0700

    Add DA_LOGIN_TC001

---
+++ TA-Dashboard/Common/TestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA_Dashboard.Common
+{
+    class TestData
+    {
+        public static string ValidUsername = "canh.tran";
+        public static string ValidPassword = "123";
+    }
+}
