011e72daccf4eb44977c47c5aa97f6aed3b244b7
ngantruong1801 <truongkimngan1801@gmail.com>
2016-08-05 13:37:54 +0700

    edit

--- TA-Dashboard/TestCases/Login.cs
+++ TA-Dashboard/TestCases/Login.cs
@@ -9,17 +9,25 @@ namespace TA_Dashboard.TestCases
     [TestClass]
     public class Login: BaseTest
     {
+        LoginPage loginPage = new LoginPage();
         [TestMethod]
         public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_via_Dashboard_login_page_with_correct_credentials()
         {
             //1. Navigate to Dashboard login page
             NavigateTADashboard();
-            LoginPage loginPage = new LoginPage();
             loginPage.Login(TestData.defaulRepository, TestData.validUsername, TestData.validPassword);
             MainPage mainPage = new MainPage();
             string actualText = mainPage.GetWelcomeText();
             Console.WriteLine(actualText);
             CheckTextDisplays(TestData.validUsername, actualText);
+        }
+
+        [TestMethod]
+        public void DA_LOGIN_TC002_Verify_that_user_fails_to_login_specific_repository_successfully_via_Dashboard_login_page_with_incorrect_credentials()
+        {
+            loginPage.Login(TestData.defaulRepository, TestData.invalidUsername, TestData.invalidPassword);
+            string actualMessage = loginPage.GetTextPopup();
+            CheckTextDisplays(TestData.errorLoginMessage, actualMessage);
         }
     }
 }
