20b5d305991e17a9dc5de7876e912e8bdb7f31d8
nguyenthingocdieu <ngocdieunguyen.ece@gmail.com>
2016-08-05 10:59:50 +0700

    Update

--- TA-Dashboard/PageObjects/MainPage.cs
+++ TA-Dashboard/PageObjects/MainPage.cs
@@ -10,7 +10,14 @@ namespace TA_Dashboard.PageObjects
     public class MainPage:GeneralPage
     {
         static readonly By _tabUser = By.XPath("//a[@href='#Welcome']");
+        static readonly By _tabRepository = By.XPath("//a[@href='#Repository']");
         static readonly By _tabAdminister = By.XPath("//a[@href='#Administer']");
-
+        static readonly By _tabGlobalSetting = By.ClassName("mn-setting");
+        static readonly By _subTabAddPage = By.XPath("a[.='Add Page']");
+        static readonly By _subTabCreateProfile = By.XPath("a[.='Create Profile']");
+        static readonly By _subTabCreatePanel = By.XPath("a[.='Create Panel']");
+        static readonly By _btnChoosePanel = By.Id("btnChoosepanel");
+        static readonly By _tabOverview = By.XPath("a[.='Overview']");
+        static readonly By _tabExecutionDashboard = By.XPath("a[.='Execution Dashboard']");
     }
 }
