f4ccba5a6cb23e52d5d1a8f3e167149db15809e9
ngantruong1801 <truongkimngan1801@gmail.com>
2016-08-05 09:26:11 +0700

    edit

--- TA-Dashboard/PageObjects/General.cs
+++ TA-Dashboard/PageObjects/General.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TA_Dashboard.Common;
+using OpenQA.Selenium;
 
 namespace TA_Dashboard.PageObjects
 {
-    class General
+    public class General
     {
+        public IWebElement FindWebElement(By locator)
+        {
+            return Constant.driver.FindElement(locator);
+        }
+        public void Click(By locator)
+        {
+            FindWebElement(locator).Click();
+        }
+        public void EnterValue(By locator, string value)
+        {
+            FindWebElement(locator).Clear();
+            FindWebElement(locator).SendKeys(value);
+        }
+        public string GetTextControl(By locator)
+        {
+            return Constant.driver.FindElement(locator).Text;
+        }
+        public void ConfirmPopup()
+        {
+            Constant.driver.SwitchTo().Alert().Accept();
+        }
+        public string GetTextPopup()
+        {
+            return Constant.driver.SwitchTo().Alert().Text;
+        }
+        
     }
 }
