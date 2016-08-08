816dd189f2182cebb734021d8867e3e7aee5362b
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2016-08-08 08:11:10 +0700

    1

--- TA-Dashboard/TestCases/Login.cs
+++ TA-Dashboard/TestCases/Login.cs
@@ -13,7 +13,6 @@ namespace TA_Dashboard.TestCases
         [TestMethod]
         public void DA_LOGIN_TC001_Verify_that_user_can_login_specific_repository_successfully_via_Dashboard_login_page_with_correct_credentials()
         {
-            //1. Navigate to Dashboard login page
             NavigateTADashboard();
             loginPage.Login(TestData.defaulRepository, TestData.validUsername, TestData.validPassword);
             MainPage mainPage = new MainPage();
@@ -24,7 +23,6 @@ namespace TA_Dashboard.TestCases
         [TestMethod]
         public void DA_LOGIN_TC002_Verify_that_user_fails_to_login_specific_repository_successfully_via_Dashboard_login_page_with_incorrect_credentials()
         {
-            NavigateTADashboard();
             loginPage.Login(TestData.defaulRepository, TestData.invalidUsername, TestData.invalidPassword);
             string actualMessage = loginPage.GetTextPopup();
             Console.WriteLine(actualMessage);
@@ -34,7 +32,6 @@ namespace TA_Dashboard.TestCases
         [TestMethod]
         public void DA_LOGIN_TC003_Verify_that_user_fails_with_correct_username_and_incorrect_password()
         {
-            NavigateTADashboard(); 
             loginPage.Login(TestData.defaulRepository, TestData.validUsername, TestData.invalidPassword);
             string actualMessage = loginPage.GetTextPopup();
             CheckTextDisplays(TestData.errorLoginMessage, actualMessage);
