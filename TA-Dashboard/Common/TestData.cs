937d37c69844234c351c9d83ead383cbbcc7a3b0
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2016-08-09 09:42:15 +0700

    1

--- TA-Dashboard/Common/TestData.cs
+++ TA-Dashboard/Common/TestData.cs
@@ -20,9 +20,9 @@ namespace TA_Dashboard.Common
         public static string dashBoardURL = listdata[1];
 
         #region Variables
-        public static string validUsername = "administrator";
-        public static string validPassword = "";
-        public static string dashBoardURL = "http://192.168.189.231:54001/TADashboard/";
+        //public static string validUsername = "administrator";
+        //public static string validPassword = "";
+        //public static string dashBoardURL = "http://192.168.189.231:54001/TADashboard/";
         public static string defaulRepository = "SampleRepository";
         public static string testRepository = "TestRepository";
         public static string invalidUsername = "abc";
